7874a373365e46fa0cd051a68991a28d949093e0
shenJakobs <148413690+shenJakobs@users.noreply.github.com>
2024-02-26 18:14:19 -0700

    Poly Assingment

---
+++ PolymorphismAssingment/PolymorphismAssingment/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismAssingment
+{
+    public class Employee : IQuittable 
+    {
+        // Properties
+        public string Name { get; set; }
+        public int EmployeeID { get; set; }
+
+        // Constructor
+        public Employee(string name, int employeeID)
+        {
+            Name = name;
+            EmployeeID = employeeID;
+        }
+
+        public Employee(string v)
+        {
+        }
+
+        // Method
+        public virtual void DisplayDetails()
+        {
+            Console.WriteLine($"Employee Name: {Name}");
+            Console.WriteLine($"Employee ID: {EmployeeID}");
+        }
+        public void Quit()
+        {
+            Console.WriteLine($"{Name} has quit the job.");
+        }
+    }
+
+}
+
---
+++ PolymorphismAssingment/PolymorphismAssingment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismAssingment
+{
+    class Program
+    {
+ 
+            static void Main(string[] args)
+            {
+                // Create an object of type IQuittable
+                Employee employee = new Employee("JOHN DOE, 1001"); // Create an Employee object using the default constructor
+
+                // Set the Name and EmployeeID properties separately
+                employee.Name = "John Doe";
+                employee.EmployeeID = 1001;
+
+
+                // Call the Quit method using the IQuittable reference
+                employee.Quit();
+
+            Console.ReadLine();
+            }
+        }
+
+    }
+
