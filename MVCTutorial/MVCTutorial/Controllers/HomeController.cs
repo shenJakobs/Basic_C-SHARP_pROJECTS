6b23ab388b2fc9729a4c3466e9b5d176114db76c
shenJakobs <148413690+shenJakobs@users.noreply.github.com>
2024-04-01 15:28:04 -0600

    Assingments

---
+++ MVCTutorial/MVCTutorial/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCTutorial.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //string text = "Hello";
+            //System.IO.File.WriteAllText(@"C:Users\Shen\documnets\log.txt", text);
+            //Random rnd = new Random(10);
+            //var num = rnd.Next();
+
+            //if (num > 2000)
+            //{
+            //    return View("About");
+            //}
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
